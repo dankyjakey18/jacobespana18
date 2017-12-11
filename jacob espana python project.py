e6c5fbd1129cfb9cd6da074a2f94049971bcfbc7
Jacob Espana <jacobespanaugt@gmsil.com>
2017-12-11 12:13:51 -0800

    Start Adventure

---
+++ jacob espana python project.py
@@ -0,0 +1,24 @@
+print('''you the scared night guard in an abandoned mansion in the middle of a forest,
+you see a ghost down the hall. What do you do?''')
+
+print('do you turn left and go into the closet, or do you turn right and jump out the window?')
+      
+turn = input(' type left or right: ')
+
+if turn == 'left':
+    print('you enter the closet and you find a flash light and a dead battery')
+    turn2 = input()
+    if turn2 == "":
+        print()
+    else:
+        print()
+    #you need to find batterys.
+elif turn == 'right':
+    print('you just jumped out the window and landed in a dumpster that is too dark to see, but found a knife')
+    #what do you use the knife for.
+else:
+    quit
+
+
+print(
+
